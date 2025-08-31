02ec1d80e37134b73ecf537285d474b91713f913
DeadHeadSean <83905762+DeadHeadSean@users.noreply.github.com>
2025-08-31 17:34:26 +0300

    Refine testimonial slider and add read-more modal

--- assets/js/het-tstm.js
+++ assets/js/het-tstm.js
@@ -1,25 +1,33 @@
 (function($){
     function initSwiper(scope){
-      $(scope).find('.het-tstm--slider .het-swiper').each(function(){
-        var $wrap = $(this).closest('.het-tstm--slider');
-        var gap = parseInt(getComputedStyle($wrap[0]).getPropertyValue('--het-gap')) || 24;
-        new Swiper(this, {
-          slidesPerView: 'auto',     // רוחב נשלט ב-CSS (כולל is-double)
-          spaceBetween: gap,
-          slidesOffsetBefore: gap,
-          slidesOffsetAfter: gap,
-          slidesPerGroupAuto: true,  // לדלג נכון על כרטיס כפול
-          navigation: {
-            nextEl: $wrap.find('.het-next')[0],
-            prevEl: $wrap.find('.het-prev')[0],
-          }
+        $(scope).find('.het-tstm--slider .het-swiper').each(function(){
+          var $wrap = $(this).closest('.het-tstm--slider');
+          var gap = parseInt(getComputedStyle($wrap[0]).getPropertyValue('--het-gap')) || 24;
+          new Swiper(this, {
+            slidesPerView: 'auto',
+            spaceBetween: gap,
+            navigation: {
+              nextEl: $wrap.find('.het-next')[0],
+              prevEl: $wrap.find('.het-prev')[0],
+            }
+          });
+        });
+      }
+
+      $(function(){
+        initSwiper(document);
+
+        $(document).on('click', '.het-tstm-more', function(){
+          var full = $(this).next('.het-tstm-full').html();
+          var $modal = $('<div class="het-tstm-modal"><div class="het-tstm-modal-content"><button class="het-tstm-modal-close" aria-label="Close">&times;</button>'+ full +'</div></div>');
+          $('body').append($modal);
+        });
+
+        $(document).on('click', '.het-tstm-modal-close, .het-tstm-modal', function(e){
+          if(e.target !== this && !$(e.target).hasClass('het-tstm-modal-close')) return;
+          $(this).closest('.het-tstm-modal').remove();
         });
       });
-    }
-
-    $(function(){
-      initSwiper(document);
-    });
 
     window.addEventListener('elementor/frontend/init', function(){
       elementorFrontend.hooks.addAction('frontend/element_ready/shortcode.default', function($scope){
